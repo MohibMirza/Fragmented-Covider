78c9c9d68e60ed2db6241fb17bd77c1773078fd1
Rijul Raghu <jraghu@usc.edu>
2022-03-27 17:49:44 -0700

    login/register pages

---
+++ app/src/main/java/edu/mohibmir/covider/Login.java
@@ -0,0 +1,153 @@
+package edu.mohibmir.covider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.core.widget.NestedScrollView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import edu.mohibmir.covider.redis.RedisDatabase;
+import edu.mohibmir.covider.redis.RClass.User;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private final AppCompatActivity activity = Login.this;
+    private NestedScrollView nestedScrollView;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private Button b;
+    private AppCompatTextView textViewLinkRegister;
+    private android.content.Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        getSupportActionBar().hide();
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+        b = (Button) findViewById(R.id.appCompatButtonLogin);
+        textViewLinkRegister = (AppCompatTextView) findViewById(R.id.textViewLinkRegister);
+        b.setOnClickListener(this);
+        textViewLinkRegister.setOnClickListener(this);
+    }
+
+    /**
+     * method to check InputEditText filled .
+     *
+     * @param textInputEditText
+     * @param textInputLayout
+     * @param message
+     * @return
+     */
+    public boolean isInputEditTextFilled(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty()) {
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        } else {
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+    /**
+     * method to check InputEditText has valid email .
+     *
+     * @param textInputEditText
+     * @param textInputLayout
+     * @param message
+     * @return
+     */
+    public boolean isInputEditTextEmail(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
+        String value = textInputEditText.getText().toString().trim();
+        if (value.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText);
+            return false;
+        } else {
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+    public boolean isInputEditTextMatches(TextInputEditText textInputEditText1, TextInputEditText textInputEditText2, TextInputLayout textInputLayout, String message) {
+        String value1 = textInputEditText1.getText().toString().trim();
+        String value2 = textInputEditText2.getText().toString().trim();
+        if (!value1.contentEquals(value2)) {
+            textInputLayout.setError(message);
+            hideKeyboardFrom(textInputEditText2);
+            return false;
+        } else {
+            textInputLayout.setErrorEnabled(false);
+        }
+        return true;
+    }
+    /**
+     * method to Hide keyboard
+     *
+     * @param view
+     */
+    private void hideKeyboardFrom(View view) {
+        android.view.inputmethod.InputMethodManager imm = (android.view.inputmethod.InputMethodManager) context.getSystemService(activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+    /**
+     * This implemented method is to listen the click on view
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.appCompatButtonLogin:
+                verifyFromRedis();
+                break;
+            case R.id.textViewLinkRegister:
+                // Navigate to RegisterActivity
+                Intent intentRegister = new Intent(getApplicationContext(), Register.class);
+                startActivity(intentRegister);
+                break;
+        }
+    }
+
+    /**
+     * This method is to validate the input text fields and verify login credentials from SQLite
+     */
+    private void verifyFromRedis() {
+        if (!isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, "Enter valid email")) {
+            return;
+        }
+        if (!isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, "Enter valid email")) {
+            return;
+        }
+        if (!isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, "Enter valid email")) {
+            return;
+        }
+        String id = textInputEditTextEmail.getText().toString();
+        User u = RedisDatabase.getOrCreateUser(id);
+        String pass = textInputEditTextPassword.getText().toString();
+
+        if (u.getPassword() == pass) {
+
+        }
+        else {
+            // Snack Bar to show success message that record is wrong
+            Snackbar.make(nestedScrollView, "Wrong email or password", Snackbar.LENGTH_LONG).show();
+        }
+    }
+    /**
+     * This method is to empty all input edit text
+     */
+    private void emptyInputEditText() {
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+    }
+}
---
+++ app/src/main/java/edu/mohibmir/covider/Register.java
@@ -0,0 +1,117 @@
+package edu.mohibmir.covider;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import androidx.core.widget.NestedScrollView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import androidx.appcompat.widget.AppCompatTextView;
+import android.view.View;
+
+import edu.mohibmir.covider.redis.RClass.User;
+import edu.mohibmir.covider.redis.RedisDatabase;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    private final AppCompatActivity activity = Register.this;
+    private NestedScrollView nestedScrollView;
+    private TextInputLayout textInputLayoutFirstName;
+    private TextInputLayout textInputLayoutLastName;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
+    private TextInputEditText textInputEditTextFirstName;
+    private TextInputEditText textInputEditTextLastName;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private TextInputEditText textInputEditTextConfirmPassword;
+    private Button buttonRegister;
+    private CheckBox cb;
+    private AppCompatTextView appCompatTextViewLoginLink;
+    private Login lg;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+        getSupportActionBar().hide();
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+        textInputLayoutFirstName = (TextInputLayout) findViewById(R.id.textInputLayoutFirstName);
+        textInputLayoutLastName = (TextInputLayout) findViewById(R.id.textInputLayoutLastName);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
+        textInputLayoutConfirmPassword = (TextInputLayout) findViewById(R.id.textInputLayoutConfirmPassword);
+        textInputEditTextFirstName = (TextInputEditText) findViewById(R.id.textInputEditTextFirstName);
+        textInputEditTextLastName = (TextInputEditText) findViewById(R.id.textInputEditTextLastName);
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+        textInputEditTextConfirmPassword = (TextInputEditText) findViewById(R.id.textInputEditTextConfirmPassword);
+        buttonRegister = (Button) findViewById(R.id.appCompatButtonRegister);
+        cb = (CheckBox) findViewById(R.id.checkbox_instructor);
+        appCompatTextViewLoginLink = (AppCompatTextView) findViewById(R.id.appCompatTextViewLoginLink);
+        buttonRegister.setOnClickListener(this);
+        cb.setOnClickListener(this);
+        appCompatTextViewLoginLink.setOnClickListener(this);
+    }
+    /**
+     * This implemented method is to listen the click on view
+     *
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.appCompatButtonRegister:
+                addToRedis();
+                break;
+            case R.id.appCompatTextViewLoginLink:
+                finish();
+                break;
+        }
+    }
+    /**
+     * This method is to validate the input text fields and post data to SQLite
+     */
+    private void addToRedis() {
+        if (!lg.isInputEditTextFilled(textInputEditTextFirstName, textInputLayoutFirstName, "Enter First Name")) {
+            return;
+        }
+        if (!lg.isInputEditTextFilled(textInputEditTextLastName, textInputLayoutLastName, "Enter Last Name")) {
+            return;
+        }
+        if (!lg.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, "Enter Valid Email")) {
+            return;
+        }
+        if (!lg.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, "Enter Valid Email")) {
+            return;
+        }
+        if (!lg.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, "Enter Password")) {
+            return;
+        }
+        if (!lg.isInputEditTextMatches(textInputEditTextPassword, textInputEditTextConfirmPassword,
+                textInputLayoutConfirmPassword, "Passwords don't match")) {
+            return;
+        }
+        User u = RedisDatabase.getOrCreateUser(textInputEditTextEmail.getText().toString().trim());
+        if (textInputEditTextPassword.getText().toString().trim() == u.getPassword()) {
+            Snackbar.make(nestedScrollView, "Email already exists", Snackbar.LENGTH_LONG).show();
+        }
+        else {
+            u.setFirstName(textInputEditTextFirstName.getText().toString().trim());
+            u.setLastName(textInputEditTextLastName.getText().toString().trim());
+            u.setPassword(textInputEditTextPassword.getText().toString().trim());
+            if (cb.isChecked()) {
+                u.setIsInstructor(true);
+            }
+            // Snack Bar to show success message that record saved successfully
+            Snackbar.make(nestedScrollView, "Registration successful", Snackbar.LENGTH_LONG).show();
+            textInputEditTextFirstName.setText(null);
+            textInputEditTextLastName.setText(null);
+            textInputEditTextEmail.setText(null);
+            textInputEditTextPassword.setText(null);
+            textInputEditTextConfirmPassword.setText(null);
+            cb.setChecked(false);
+        }
+    }
+}
